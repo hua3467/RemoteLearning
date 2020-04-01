9da51a40350f6a9675d69baf4790a6b2f0931ea2
Aaron Yang <zhenhuayang@outlook.com>
2020-04-01 09:47:34 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -3,21 +3,30 @@ const name = document.querySelector("#name");
 const signInPan = document.querySelector('#signIn');
 const player = document.querySelector('#player');
 const cover = document.querySelector('#cover');
-const menu = document.querySelectorAll('nav a');
+const menuList = document.querySelector('ul');
 let btnFlag = false;
 let userName
 
 const lectureUrls = [
-    'https://www.youtube.com/embed/JMRsNRveMtc',    //Lecture-Mobile
-    'https://www.youtube.com/embed/G8hL8ULlwY8',    //Demo-Hover
-    'https://www.youtube.com/embed/8DQID1cNLT4'     //Demo-Assignment
+    'https://www.youtube.com/embed/JMRsNRveMtc', //Lecture-Mobile
+    'https://www.youtube.com/embed/G8hL8ULlwY8', //Demo-Hover
+    'https://www.youtube.com/embed/8DQID1cNLT4' //Demo-Assignment
 ]
 
-const videoUrls = {
-    'Lecture: Responsive Design and Mobile UI': 'https://www.youtube.com/embed/JMRsNRveMtc',
-    'Adobe XD Demo 1: Hover and Component State': 'https://www.youtube.com/embed/G8hL8ULlwY8',
-    'Project Demo: App Store Clone': 'https://www.youtube.com/embed/8DQID1cNLT4'
-}
+const videos = [{
+        title: 'Lecture: Responsive Design and Mobile UI',
+        url: 'https://www.youtube.com/embed/JMRsNRveMtc'
+    },
+    {
+        title: 'Adobe XD Demo 1: Hover and Component State',
+        url: 'https://www.youtube.com/embed/G8hL8ULlwY8'
+    },
+    {
+        title: 'Project Demo: App Store Clone',
+        url: 'https://www.youtube.com/embed/8DQID1cNLT4'
+    }
+
+]
 
 var firebaseConfig = {
     apiKey: "AIzaSyCstqaNTffHq57R459MRQTbsBPvL_C7mtU",
@@ -33,15 +42,26 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 const dataname = 'remoteLearning'
 
-for(let i = 0; i < menu.length - 2; i++){
-    menu[i].addEventListener('click', function(e){
-        player.src = lectureUrls[i];
+for(let i = 0; i < videos.length; i++){
+    let newLi = document.createElement('li');
+    let newA = document.createElement('a');
+    newA.innerHTML = videos[i].title;
+    newA.href = '';
+    newLi.appendChild(newA);
+    menuList.appendChild(newLi);
+}
+
+const menu = document.querySelectorAll('nav a');
+
+for (let i = 0; i < videos.length; i++) {
+    menu[i].addEventListener('click', function (e) {
+        player.src = videos[i].url;
         e.preventDefault();
     })
 }
 
-name.addEventListener( 'change', function(){
-    if (name.value.length < 2){
+name.addEventListener('change', function () {
+    if (name.value.length < 2) {
         btnStart.style.background = '#999999';
         btnFlag = false;
     } else {
@@ -50,8 +70,8 @@ name.addEventListener( 'change', function(){
     }
 })
 
-btnStart.addEventListener('click', function(){
-    if(btnFlag === true){
+btnStart.addEventListener('click', function () {
+    if (btnFlag === true) {
         let date = new Date();
         let time = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear() + ', ' + date.getHours() + ':' + date.getMinutes();
         userName = name.value;
@@ -62,16 +82,16 @@ btnStart.addEventListener('click', function(){
         signInPan.style.visibility = 'hidden';
         cover.style.visibility = 'hidden';
         endTime();
-        } else {
+    } else {
         alert('Please enter your full name first.')
-    }   
+    }
 }, false)
 
-function endTime(){
+function endTime() {
 
     let count = 0;
 
-    setInterval(function(){
+    setInterval(function () {
         let time = new Date();
         console.log(time.toLocaleTimeString());
         console.log(name.value);
@@ -79,11 +99,11 @@ function endTime(){
             'endTime': time.getHours() + ':' + time.getMinutes()
         });
         count++;
-        }, 300000);
-        
+    }, 300000);
+
 }
-  
-function stopTimer(){
-timeEnd = new Date();
-timeStamp = timeEnd - timeStart;
+
+function stopTimer() {
+    timeEnd = new Date();
+    timeStamp = timeEnd - timeStart;
 }
