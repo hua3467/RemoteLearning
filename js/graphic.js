e14ebcb159d38d43c174f3f8a02a8c7b46c3206c
Aaron Yang <zhenhuayang@outlook.com>
2020-05-15 09:57:58 -0500

    update

---
+++ js/graphic.js
@@ -0,0 +1,47 @@
+const figure_info = [
+    {
+        image: "/images/figureDrawing/BandIzakMoleterno.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/Edwin Degges.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/Isolation.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/JackieChan.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/Maeve.pdf"
+    }, 
+    {
+        image: "/images/figureDrawing/MultiFigureNarrative.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/MultipleFigure.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/PALS2.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/Rosenkranz.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/SarthakKaushik.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/SelfPortrait.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/selfportraitedited.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/SeminoleScizophrenic.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/swenson.jpg"
+    }, 
+    {
+        image: "/images/figureDrawing/Tamika.jpg"
+    }, 
+]
